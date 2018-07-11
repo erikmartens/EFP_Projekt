95f62efdcd632607147016d88271cd3d718ffb5c
erikmartens <erikmartens@me.com>
2018-07-11 14:12:24 +0200

    markdown parsing for telegram messages

--- backend/src/chatbot/telegram_bot.clj
+++ backend/src/chatbot/telegram_bot.clj
@@ -7,4 +7,4 @@
 
 (defn botapi [{{id :id} :chat text :text}]
   (let [{question :question intent :intent answer :answer} (chatbot.recognition/answer text)]
-    (api/send-text token id answer)))
+    (api/send-text token id {:parse_mode "Markdown"} answer)))
